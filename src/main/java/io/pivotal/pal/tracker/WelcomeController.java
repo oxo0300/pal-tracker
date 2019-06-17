182b8a8a7fb387160d0a5269dc61e9695cbc8594
Omololu Ojo <ojo@sigmastudioz.com>
2019-06-17 16:50:22 -0400

    Adding changes for cloudNativeDeveloperSimpleApp

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @Value("${welcome.message}")
+    String helloMsg;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return helloMsg;
+    }
+}
